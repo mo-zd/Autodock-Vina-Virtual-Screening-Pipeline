03a55bae883d7cef385a04f705bf5533dbf1e424
Mohsen Yazdani <91246296+mo-zd@users.noreply.github.com>
2023-04-05 00:03:38 +0330

    Update VinaScreen.py

--- VinaScreen.py
+++ VinaScreen.py
@@ -1,55 +1,121 @@
 import os
 import pandas as pd
+import tkinter as tk
+from tkinter import filedialog
 from config_generator import generate_config_file
 from complex_generator import generate_complex
 from result_parser import parse_vina_result
 from vina_executor import run_vina
+from tkinter import messagebox
+import traceback
 
-try:
-    # ask for user inputs
-    ligand_path = input("Enter the path of ligands in PDBQT format: ")
-    protein_path_folder = input("Enter the folder path of protein in PDBQT format: ")
-    box_size = input("Enter the size of the box: ")
-    center = input("Enter the center of the box: ")
-    output_path = input("Enter the output path for VINA results: ")
-    vina_path = input("Enter the path of Autodock Vina executable: ")
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.master.geometry("500x500")
+        self.master.title("Autodock Vina Virtual Screening")
+        self.create_widgets()
 
-    # find protein file in the folder
-    protein_path = None
-    for file in os.listdir(protein_path_folder):
-        if file.endswith(".pdbqt"):
-            protein_path = os.path.join(protein_path_folder, file)
-            break
-    if protein_path is None:
-        raise Exception("No protein file found in the provided folder.")
+    def create_widgets(self):
+        # Ligand file selection
+        self.ligand_label = tk.Label(self.master, text="Select Ligand File")
+        self.ligand_label.pack()
+        self.ligand_button = tk.Button(self.master, text="Browse", command=self.select_ligand_file)
+        self.ligand_button.pack()
 
-    # Generate configuration file
-    generate_config_file(protein_path, box_size, center)
+        # Protein file selection
+        self.protein_label = tk.Label(self.master, text="Select Protein Folder")
+        self.protein_label.pack()
+        self.protein_button = tk.Button(self.master, text="Browse", command=self.select_protein_folder)
+        self.protein_button.pack()
 
-    # perform virtual screening with Vina
-    for file in os.listdir(ligand_path):
-        if file.endswith(".pdbqt"):
-            ligand_file = os.path.join(ligand_path, file)
-            result_file = os.path.join(output_path, file.split(".")[0] + "_result.pdbqt")
-            run_vina(vina_path, "config.txt", ligand_file, result_file)
+        # Box size input
+        self.box_label = tk.Label(self.master, text="Enter Box Size")
+        self.box_label.pack()
+        self.box_entry = tk.Entry(self.master)
+        self.box_entry.pack()
 
-    # read VINA results
-    results_df = pd.DataFrame(columns=["Ligand Name", "VINA RESULT"])
-    for file in os.listdir(output_path):
-        if file.endswith("_result.pdbqt") or file.endswith("_out.pdbqt"):
-            ligand_name = file.split("_result.pdbqt")[0].split("_out.pdbqt")[0]
-            result_file = os.path.join(output_path, file)
-            vina_result = parse_vina_result(result_file)
-            results_df = results_df.append({"Ligand Name": ligand_name, "VINA RESULT": vina_result}, ignore_index=True)
+        # Box center input
+        self.center_label = tk.Label(self.master, text="Enter Box Center")
+        self.center_label.pack()
+        self.center_entry = tk.Entry(self.master)
+        self.center_entry.pack()
 
-    # sort results by VINA RESULT and find top 5 conformers
-    results_df = results_df.sort_values("VINA RESULT", ascending=False)
-    top_conformers = list(results_df["Ligand Name"].head(5))
+        # Output path selection
+        self.output_label = tk.Label(self.master, text="Select Output Folder")
+        self.output_label.pack()
+        self.output_button = tk.Button(self.master, text="Browse", command=self.select_output_folder)
+        self.output_button.pack()
 
-    # generate PDB file for protein and conformers
-    for conformer in top_conformers:
-        # generate complex file
-        generate_complex(protein_path, os.path.join(ligand_path, conformer + ".pdbqt"), conformer, output_path)
+        # Vina path input
+        self.vina_label = tk.Label(self.master, text="Enter Vina Executable Path")
+        self.vina_label.pack()
+        self.vina_entry = tk.Entry(self.master)
+        self.vina_entry.pack()
 
-except Exception as e:
-    print("Error: ", e)
+        # Run button
+        self.run_button = tk.Button(self.master, text="Run Virtual Screening", command=self.run_virtual_screening)
+        self.run_button.pack()
+
+    def select_ligand_file(self):
+        self.ligand_path = filedialog.askdirectory()
+
+    def select_protein_folder(self):
+        self.protein_path = filedialog.askdirectory()
+
+    def select_output_folder(self):
+        self.output_path = filedialog.askdirectory()
+
+    def run_virtual_screening(self):
+        try:
+            # get user inputs
+            ligand_path = self.ligand_path
+            protein_path_folder = self.protein_path
+            box_size = self.box_entry.get()
+            center = self.center_entry.get()
+            output_path = self.output_path
+            vina_path = self.vina_entry.get()
+
+            # find protein file in the folder
+            protein_path = None
+            for file in os.listdir(protein_path_folder):
+                if file.endswith(".pdbqt"):
+                    protein_path = os.path.join(protein_path_folder, file)
+                    break
+            if protein_path is None:
+                raise Exception("No protein file found in the provided folder.")
+
+            # Generate configuration file
+            generate_config_file(protein_path, box_size, center)
+
+            # perform virtual screening with Vina
+            for file in os.listdir(ligand_path):
+                if file.endswith(".pdbqt"):
+                    ligand_file = os.path.join(ligand_path, file)
+                    result_file = os.path.join(output_path, file.split(".")[0] + "_result.pdbqt")
+                    run_vina(vina_path, "config.txt", ligand_file, result_file)
+
+            # read VINA results
+            results_df = pd.DataFrame(columns=["Ligand Name", "VINA RESULT"])
+            for file in os.listdir(output_path):
+                if file.endswith("_result.pdbqt") or file.endswith("_out.pdbqt"):
+                    ligand_name = file.split("_result.pdbqt")[0].split("_out.pdbqt")[0]
+                    result_file = os.path.join(output_path, file)
+                    vina_result = parse_vina_result(result_file)
+                    results_df = results_df.append({"Ligand Name": ligand_name, "VINA RESULT": vina_result}, ignore_index=True)
+                    
+                    # generate complex files
+                    generate_complex(protein_path, ligand_file, ligand_name, output_path)
+
+            # show success message
+            tk.messagebox.showinfo("Virtual Screening", "Virtual Screening completed successfully.")
+
+        except Exception as e:
+            # show error message
+            error_message = str(e) + '\n\n' + traceback.format_exc()
+            tk.messagebox.showerror("Error", error_message)
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
